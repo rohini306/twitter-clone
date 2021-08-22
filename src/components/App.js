325443e94e931c1ef9ea2fb1f981e86aa35af083
Rohini <rohh3006@gmail.com>
2021-08-22 17:51:15 +0530

    created a list of messages

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,21 @@ class App extends Component {
   state = {
     DarkMode: true,
     Name: "",
-    Image: ""
+    Image: "",
+    Messages: [
+      {
+        Name: "Hello",
+        Image:
+          "https://img-premium.flaticon.com/png/512/560/premium/560277.png?token=exp=1629635617~hmac=aa37c962efe4572b144558c8e2585946",
+        Text: "Hi guys"
+      },
+      {
+        Name: "Hello",
+        Image:
+          "https://img-premium.flaticon.com/png/512/560/premium/560277.png?token=exp=1629635617~hmac=aa37c962efe4572b144558c8e2585946",
+        Text: "Hi gals"
+      }
+    ]
   };
   render() {
     // eslint-disable-next-line no-unused-vars
